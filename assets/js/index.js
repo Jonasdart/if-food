cae44e7019cb5bb3e58560c08a03cfbfdfad1b05
jonasdart <duarte.jsystem@gmail.com>
2022-04-28 22:12:08 -0300

    feat: Adicionada base

---
+++ assets/js/index.js
@@ -0,0 +1,15 @@
+actualImage = 1;
+
+function nextImage(){
+    if(actualImage === 1){
+        document.getElementById("promo1").style.display = "none";
+        document.getElementById("promo2").style.display = "block";
+        actualImage = 2;
+    }else {
+        document.getElementById("promo2").style.display = "none";
+        document.getElementById("promo1").style.display = "block";
+        actualImage = 1;
+    }
+}
+
+setInterval(nextImage, 5000);
